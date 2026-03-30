1bb693154f547a9486af116040a2b98b4f1d4c02
lipe-manza <luizffrances@gmail.com>
2026-03-30 11:43:05 -0300

    adjusting new cases (5 and 4 ) and making little formatting for the printf in case 5

--- 1stProject/entrega.c
+++ 1stProject/entrega.c
@@ -1,3 +1,11 @@
+/*
+Luiz Felipe Manzoli Franceschini - 16913300
+Mateus Juares Felipe - 16891602
+Felipe Fabiano
+Juan
+*/
+
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
@@ -95,13 +103,13 @@ void add_edge(Graph *g, int v1, int v2, int w)
     g->edNum++;
 }
 
-bool exist_edge(Graph *g, int v1, int v2)
+int exist_edge(Graph *g, int v1, int v2)
 {
     // verificação padrão para ver se a entrada é valida
     if (g == NULL || v1 < 1 || v2 < 1 || v1 >= g->edNum || v2 >= g->edNum)
-        return false;
+        return 0;
 
-    return (g->matrix[v1 - 1][v2 - 1] != -1) ? true : false;
+    return (g->matrix[v1 - 1][v2 - 1] != -1) ? 1 : 0;
 }
 
 int *neighbors(Graph *g, int v1)
@@ -111,43 +119,32 @@ int *neighbors(Graph *g, int v1)
         return NULL;
 
     int count = 0;
-    int temp[g->vtNum];
+    int *temp = malloc(g->vtNum * sizeof(int *));
 
     for (int i = 0; i < g->vtNum; i++)
     {
         if (g->matrix[v1 - 1][i] != -1)
         {
-            temp[count++] = i;
-            printf("%d ", i + 1);
+            temp[count++] = i + 1;
         }
     }
 
     temp[count] = -1;
-
-    int *array = (int *)malloc(count * sizeof(int));
-
-    int j = 0;
-    while (temp[j] == -1)
-    {
-        array[j] = temp[j];
-        j++;
-    }
-    return array;
+    return temp;
 }
 
-bool remove_edge(Graph *g, int v1, int v2)
+int remove_edge(Graph *g, int v1, int v2)
 {
     if (g->matrix[v1][v2] == -1 || v1 > g->vtNum || v2 > g->vtNum)
     {
-        printf("-1");
-        return false;
+        return -1;
     }
 
     g->matrix[v1 - 1][v2 - 1] = -1;
     g->matrix[v2 - 1][v1 - 1] = -1;
 
     g->edNum--;
-    return true;
+    return 1;
 }
 
 void print_info(Graph *g)
@@ -165,6 +162,7 @@ void print_info(Graph *g)
     printf("E = [");
     int count = 0;
 
+    // Printa a diagonal de baixo da matriz no formato (coluna , linha)
     for (int j = 0; j < g->vtNum; j++)
     {
         for (int i = 0; i <= j; i++)
@@ -207,11 +205,17 @@ int max_neighbors(Graph *g)
 
     return max;
 }
+
 int **adjacency_matrix(Graph *g)
 {
     if (g == NULL)
         return NULL;
     return g->matrix;
+}
+
+int getVtNum(Graph *g)
+{
+    return g->vtNum;
 }
 
 int main(void)
@@ -221,6 +225,7 @@ int main(void)
     int res;
     bool print_status = 1;
     int *p;
+    int **m;
 
     Graph *G;
 
@@ -235,12 +240,10 @@ int main(void)
             G = MyGraph(N);
             break;
         case 1:
-            // adicionamos
             scanf("%d %d %d", &x, &y, &w);
             add_edge(G, x, y, w);
             break;
         case 2:
-            // get
             scanf("%d %d", &x, &y);
             res = exist_edge(G, x, y);
             printf("%d\n", res);
@@ -249,13 +252,41 @@ int main(void)
         case 3:
             scanf("%d", &x);
             p = neighbors(G, x);
+            for (int i = 0; i < getVtNum(G); i++)
+            {
+                if (p[i] == -1)
+                    break;
+                else
+                    printf("%d ", p[i]);
+            }
             print_status = 0;
             free(p);
             break;
         case 4:
             scanf("%d %d", &x, &y);
-            if (!remove_edge(G, x, y))
+            res = remove_edge(G, x, y);
+            if (res == -1)
+            {
+                printf("-1");
                 print_status = 0;
+            }
+            break;
+        case 5:
+            m = adjacency_matrix(G);
+            printf("Adjacency Matrix:\n");
+            for (int i = 0; i < getVtNum(G) - 1; i++)
+            {
+                printf("  "); // Da dois espaços antes de cada linha
+                for (int j = 0; j < getVtNum(G) - 1; j++)
+                {
+                    if (m[i][j] == -1)
+                        printf("0%s", (m[i][j + 1] > 9) ? "  " : "   ");
+                    else
+                        printf("%d%s", m[i][j], (m[i][j + 1] > 9) ? "  " : "   "); // 2 espaços se < 9 e 3 se > 9
+                }
+                printf("\n");
+            }
+            print_status = 0;
             break;
         default:
             printf("unrecognized option %d!\n", option);
